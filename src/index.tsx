a08d65bf83508429ded3444cc48d6e9cd9b991fa
Mikhail-Ropii <mr.ropiy@gmail.com>
2022-11-30 21:55:36 +0200

    Update Routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter basename="/test-job-board/">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
